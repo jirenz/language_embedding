5b41efa233943b2d23567ccc6a7a63eb86fbf8de
jirenz <zhujiren1155@hotmail.com>
2017-03-04 14:48:56 -0800

    prepdata

--- prepdata.sh
+++ prepdata.sh
@@ -1,3 +1,43 @@
 #!/usr/bin/bash
-#
-# python2.7 [path_to_excecutable]/WikiExtractor.py -o [path_to_output] -b 100M [path_to_input_xml]
+DATA_PATH = /datadrive/data
+
+RAW_DATA_FILE = enwiki-20170220-pages-articles-multistream.xml.bz2
+RAW_DATA_URL = https://dumps.wikimedia.org/enwiki/20170220/enwiki-20170220-pages-articles-multistream.xml.bz2
+
+WIKI_EXTRACT_PATH = WikiDump
+CHUNK_SIZE = 150M
+CHUNKED_ARTICLES_PATH = wiki_articles
+
+GRAM_W_COUNT_PATH = wiki_gram
+GRAM_W_COUNT_SIZE = 3
+
+GRAM_TRIM_PATH = wiki_gram_trimmed
+GRAM_TRIM_THRESHOLD = 2
+
+GRAM_COLLECT_PATH = wiki_gram_collected
+GRAM_COLLECT_FILE_NAME = wiki_grams
+GRAM_COLLECT_FILE = $DATA_PATH/$GRAM_COLLECT_PATH/$GRAM_COLLECT_FILE_NAME
+
+#Download files
+if [ ! -e $DATA_PATH/$RAW_DATA_FILE]; then
+	wget $RAW_DATA_URL -O $DATA_PATH/$RAW_DATA_FILE
+fi
+#Chunk data
+if [ ! -e $DATA_PATH/$CHUNKED_ARTICLES_PATH/AA/wiki_00]; then
+	python $WIKI_EXTRACT_PATH/WikiExtractor.py -o $DATA_PATH/$CHUNKED_ARTICLES_PATH -b $CHUNK_SIZE $DATA_PATH/$RAW_DATA_FILE
+fi
+# Count vocab
+if [ ! -e $DATA_PATH/$GRAM_W_COUNT_PATH/wiki_00*]; then
+	python grams_w_count.py -n $GRAM_W_COUNT_SIZE -o $DATA_PATH/$GRAM_W_COUNT_PATH $DATA_PATH/$CHUNKED_ARTICLES_PATH/AA/*
+fi
+# Trim vocab
+if [ ! -e $DATA_PATH/$GRAM_TRIM_PATH/wiki_00*]; then
+	python grams_trim.py -t $GRAM_TRIM_THRESHOLD -o $DATA_PATH/$GRAM_TRIM_PATH $DATA_PATH/$GRAM_W_COUNT_PATH/*
+fi
+# Collect vocab
+if [ ! -e $GRAM_COLLECT_FILE]; then
+	python grams_reduce.py -t $GRAM_TRIM_THRESHOLD -o $GRAM_COLLECT_FILE $DATA_PATH/$GRAM_TRIM_PATH/*
+fi
+
+# Reduce to one vocab file
+python grams_reduce.py
