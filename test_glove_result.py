04b8df11d462590829a29d007d72fee8dbe0ff32
pearfish16 <yuetong@stanford.edu>
2017-03-07 18:43:30 -0800

    finish test_glove_result; no testing

--- test_glove_result.py
+++ test_glove_result.py
@@ -6,16 +6,24 @@ from os import listdir
 from os.path import isfile, join, basename, splitext
 import random
 import argparse
-from helper import 
+from helper import load_data_from_json
 
+parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--inputfile', default='./data/glove_mar6/vectors.txt', type=str, help='input file')
 parser.add_argument('-l', '--labelgram', default='./Tokenized/label_gram.json', type=str, help='output file')
 args = parser.parse_args()
 print(args)
 
-inputfile = args.inputfile;
-labelgram = args.labelgram;
-
 Dict = {}
+load_data_from_json(Dict, args.labelgram)
 
-
+count = [0, 0, 0, 0, 0, 0]
+with open(args.inputfile) as F:
+	for line in F:
+		idx = (line.split(" "))[0]
+		gram = Dict[idx]
+		len_gram = len(gram.split(" "))
+		count[len_gram] += 1
+for i in range(1, 4):
+	print str(i) + "-gram appear", count[i], "times."
+print "Total:", sum(count), "times."
