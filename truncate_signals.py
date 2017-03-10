0aef6e8f99a161b950dd55a80ffe8d1232ad06a7
Ubuntu <project@jz-yw-jm.esagqdd40vietgsw2qqb1yqtkg.bx.internal.cloudapp.net>
2017-03-10 18:11:34 +0000

    suppress gram

--- truncate_signals.py
+++ truncate_signals.py
@@ -1,6 +1,7 @@
 import sys
 
-num_grams = 60000
+num_words = 60000
+num_grams = 20000
 num_synsets = 20000
 num_tags = 100
 
@@ -32,10 +33,22 @@ def read_file(f):
 	tags.reverse()
 	return grams, synsets, tags
 
-def dump_ref(f, arr, count):
+def filter_n_grams(grams):
+	words = []
+	n_grams = []
+	for gram in grams:
+		token, count = gram
+		token = token.strip()
+		if token.find(' ') >= 0:
+			n_grams.append((token, count))
+		else:
+			words.append((token, count))
+	return words, grams
+
+def dump_ref(f, arr, count, offset = 0):
 	for index in xrange(count):
 		token, _ = arr[index]
-		f.write('{},{}\n'.format(index, token))
+		f.write('{},{}\n'.format(index + offset, token))
 
 
 f = open(sys.argv[1], 'r')
@@ -46,16 +59,21 @@ sample1, sample1_count = grams[0]
 sample2, sample2_count = grams[1]
 assert sample1_count > sample2_count
 
+words, grams = filter_n_grams(grams)
+
+num_words = min(len(words), num_words)
 num_grams = min(len(grams), num_grams)
 num_synsets = min(len(synsets), num_synsets)
 num_tags = min(len(tags), num_tags)
 
+print "words:", num_words
 print "grams:", num_grams
 print "synsets:", num_synsets
 print "tags:", num_tags
 
 with open(out_path + 'n_gram_table.ref', 'w') as f_out:
-	dump_ref(f_out, grams, num_grams)
+	dump_ref(f_out, words, num_words)
+	dump_ref(f_out, grams, num_grams, offset=num_words)
 
 with open(out_path + 'synset_offset_table.ref', 'w') as f_out:
 	dump_ref(f_out, synsets, num_synsets)
